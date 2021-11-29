3b640abe2a4a409787a8198bbc6be0b70bfd977e
winstonwang <wwang@lcs.on.ca>
2021-11-29 14:25:54 -0500

    Initial Commit

---
+++ CopyAndPasteHistoryApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CopyAndPasteHistoryApp
+//
+//  Created by Winston Wang on 2021-11-29.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ CopyAndPasteHistoryApp/CopyAndPasteHistoryAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CopyAndPasteHistoryAppApp.swift
+//  CopyAndPasteHistoryApp
+//
+//  Created by Winston Wang on 2021-11-29.
+//
+
+import SwiftUI
+
+@main
+struct CopyAndPasteHistoryAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
